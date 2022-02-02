df85a3b548c0bcb291dbc16fb58ad49392508087
Umair Irfan <umaironline@live.com>
2022-02-02 11:43:26 +0500

    Initial commit + Basic Project Setup

---
+++ WeRPlayiOSTest/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeRPlayiOSTest
+//
+//  Created by Umair Irfan on 02/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
